b94232b7b3e635b00d5f75e346bc6c4048e9f9c1
Leonardo Natali <leonardo.natali@hotmail.com>
2023-06-27 16:23:22 -0300

    refact: moves current files to domain folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Address from "./entity/address"
-import Customer from "./entity/customer"
-import Order from "./entity/order"
-import OrderItem from "./entity/order_item"
+import Address from "./domain/entity/address"
+import Customer from "./domain/entity/customer"
+import Order from "./domain/entity/order"
+import OrderItem from "./domain/entity/order_item"
 
 const customer = new Customer("customer_id", "test customer")
 const address = new Address("test street", "123abc", "29730000", "test city")
